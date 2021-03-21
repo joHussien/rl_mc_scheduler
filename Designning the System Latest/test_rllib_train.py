a2eff9d7fbd25320ddedc87e4806cfff61747988
root <root@deeplearning-1-vm.c.abstract-ring-306422.internal>
2021-03-21 14:42:09 +0000

    meaningless commit

--- Designning the System Latest/test_rllib_train.py
+++ Designning the System Latest/test_rllib_train.py
@@ -162,14 +162,14 @@ if __name__ == "__main__":
 
     register_env("not_filtered_not_degraded_offline", lambda env_config: MCEnv())
     hyper_parameters={"env": "not_filtered_not_degraded_offline", "num_workers": 14, "num_cpus_per_worker": 1,
-                              "num_gpus": 0 , "horizon": 1,"lr":tune.uniform(0,1),"gamma":tune.uniform(0,1),
-                                "replay_proportion": tune.uniform(0,1),"replay_buffer_num_slots":1
+                              "num_gpus": 0 , "horizon": 1,"lr":0.0005,"gamma":0.9,
+                                "replay_proportion":1,"replay_buffer_num_slots":1
                                 # "gamma":tune.grid_search([0.1,0.5,0.9,1]),
                               # "prioritized_replay": 'true'
                       # "timesteps_per_iteration":tune.grid_search([100,500,1000])
                       }
     bayesopt = BayesOptSearch(metric="episode_reward_mean", mode="max")
-    tune.run(ImpalaTrainer, checkpoint_freq=50, stop={"training_iteration": 500},  config=hyper_parameters,search_alg=bayesopt)
+    tune.run(ImpalaTrainer, checkpoint_freq=50, stop={"training_iteration": 500},  config=hyper_parameters)
                                                                                          # "prioritized_replay":True, "timesteps_per_iteration": 500,
 
                                                                                          #})
